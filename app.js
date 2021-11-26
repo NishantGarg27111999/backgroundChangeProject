8bd6872769ca2c43e38ca63b5db926ea3271ef85
NishantGarg27111999 <95064705+NishantGarg27111999@users.noreply.github.com>
2021-11-26 20:42:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express =require("express");
 const path=require("path");
 const app=express();
-const port=Process.env.PORT ||5000;
+const port=Process.env.PORT ||3000;
 
 
 
 
 const staticpath=path.join(__dirname,'public');
 
-console.log(staticpath);
+// console.log(staticpath);
 
 
 app.use(express.static(staticpath));
