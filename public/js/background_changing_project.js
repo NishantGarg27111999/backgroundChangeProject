106adc0600aed0550db7279c7352a8bb6dd3a4bd
NishantGarg27111999 <95064705+NishantGarg27111999@users.noreply.github.com>
2021-11-27 16:57:37 +0530

    Update background_changing_project.js

--- public/js/background_changing_project.js
+++ public/js/background_changing_project.js
@@ -2,36 +2,39 @@ console.log('welcome to project');
 // let a =Math;
 // console.log(a)
 
-let hexCode;
+
 function change_color(){
+let hexCode;
 let r=15+(255-15)*Math.random();
 r=Math.floor(r)
 r=r.toString(16)
-console.log(r)
+// console.log(r)
 let g=15+(255-15)*Math.random();
 g=Math.floor(g)
 g=g.toString(16)
-console.log(g);
+// console.log(g);
 let b=15+(255-15)*Math.random();
 b=Math.floor(b);
 
 b=b.toString(16)
-console.log(b)
+// console.log(b)
 
  hexCode="#"+r+g+b;
-console.log(hexCode)
+// console.log(hexCode)
 body=document.getElementsByTagName("body");
-console.log(body);
+// console.log(body);
 body[0].style.background=hexCode;
 // body[0].style.backgroundColor=`rgb(${r},${g},${b})`;
 // console.log(`rgb(${r},${g},${b})`);
+ 
+ let btn=document.getElementById("btn");
+btn.innerText=hexCode;
 }
 change_color();
 
-let btn=document.getElementById("btn");
-btn.innerText=hexCode;
+
 btn.addEventListener("click",()=>{
-    console.log("clicked")
+//     console.log("clicked")
     change_color();
 })
 
